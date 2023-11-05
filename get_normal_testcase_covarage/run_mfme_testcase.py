380e14e34c74e202c7632351379633305ac17a7f
lin wang <wwwglin@163.com>
2023-11-05 11:49:29 +0000

    增加多个cpp文件复制

--- get_normal_testcase_covarage/run_mfme_testcase.py
+++ get_normal_testcase_covarage/run_mfme_testcase.py
@@ -11,8 +11,10 @@ import sys
 import os
 
 # 设定日志文件的路径
-log_file_path = os.path.join('/root/mfem/mfem-code-analyzer/get_normal_testcase_covarage', 'log.log')
-info_log_file_path = os.path.join('/root/mfem/mfem-code-analyzer/get_normal_testcase_covarage', 'info.log')
+log_file_path = os.path.join(
+    '/root/mfem/mfem-code-analyzer/get_normal_testcase_covarage', 'log.log')
+info_log_file_path = os.path.join(
+    '/root/mfem/mfem-code-analyzer/get_normal_testcase_covarage', 'info.log')
 
 # 如果文件存在，则移除
 if os.path.exists(log_file_path):
@@ -42,6 +44,8 @@ logger.addHandler(all_levels_handler)
 logger.addHandler(info_handler)
 
 # StreamToLogger类
+
+
 class StreamToLogger(object):
     def __init__(self, logger, log_level=logging.INFO):
         self.logger = logger
@@ -54,6 +58,7 @@ class StreamToLogger(object):
 
     def flush(self):
         pass
+
 
 # 重定向stdout和stderr到日志
 sys.stdout = StreamToLogger(logger, logging.DEBUG)  # 注意这里改成了DEBUG
@@ -89,18 +94,14 @@ class MFEMIssueProcessor:
         url = issue_data.get("url", "")
 
         # 根据issue_name 定位测试用例文件位置
-
-        # 根据testcase 定位文件位置
-        # 你可以在这里添加进一步的逻辑来处理文件位置
-
-        # 根据test_case是否为空来判断是否为正常的issue
-        self.copy_testcase(name, testcase)
         # 运行bug版本
         logger.info(f"Running bug version: {bug_sha}")
         self.switch_version(bug_sha)
-        self.copy_testcase(name, testcase)
+        for tc in testcase.split(","):
+            self.copy_testcase(name, tc)
         self.compile_mfem()
-        testnames=testname.split(",")
+        testnames = testname.split(",")
+
         for testname in testnames:
             self.run_unit_test(testname)
 
@@ -137,17 +138,17 @@ class MFEMIssueProcessor:
                 print(f"Processing {issue_name}")
                 if self.is_get_normal_testcase:
                     # 提取测试用例
-                    self.pre_issue_file(issue_name, issue_data)
+                    testcase_paths = issue_data.get("testcase", "").split(",")
+                    for testcase_path in testcase_paths:
+                        self.pre_issue_file(
+                            issue_name, issue_data, testcase_path)
                 # 将issue_name和issue_data传递给process_mfem_issue函数
                 self.process_mfem_issue(issue_data)
 
-    def pre_issue_file(self, issue_name, issue_data):
+    def pre_issue_file(self, issue_name, issue_data, testcase_path):
         # 获取Git SHA for fix
         fix_sha = issue_data.get("fix_sha", "")
         self.switch_version(fix_sha)
-
-        # 获取测试用例路径
-        testcase_path = issue_data.get("testcase", "")
         if not testcase_path:
             print(f"Test case path is not specified for {issue_name}")
             return
@@ -195,7 +196,7 @@ class MFEMIssueProcessor:
                 # 删除所有未跟踪文件
                 subprocess.check_call(["git", "clean", "-fd"])
 
-                 # 切换到master分支
+                # 切换到master分支
                 subprocess.check_call(["git", "checkout", "master"])
 
                 # 从完整的SHA生成短SHA
@@ -237,32 +238,34 @@ class MFEMIssueProcessor:
             print(f"An error occurred while completing mfem: {e}")
             logger.error(f"An error occurred while completing mfem: {e}")
 
-
     # 复制测试用例
-    def copy_testcase(self,name, testcase):
-        source_path = os.path.join(self.bugs_path, name, testcase.split("/")[-1])
+    def copy_testcase(self, name, testcase):
+        source_path = os.path.join(
+            self.bugs_path, name, testcase.split("/")[-1])
         target_path = os.path.join(self.mfem_path, testcase)
         shutil.copy(source_path, target_path)
         print(f"Copied fix file {source_path} to {target_path}")
 
         pass
 
-
     # 运行unit test中的测试用例
+
     def run_unit_test(self, testname):
         cmd_path = os.path.join(self.mfem_path, "build/tests/unit")
         os.chdir(cmd_path)
 
         try:
             # 使用capture_output来捕获输出
-            result = subprocess.run(["/root/mfem/mfem-code-analyzer/get_normal_testcase_covarage/run_tesscase.sh", testname], capture_output=True, text=True)
+            result = subprocess.run(
+                ["/root/mfem/mfem-code-analyzer/get_normal_testcase_covarage/run_tesscase.sh", testname], capture_output=True, text=True)
             # 将标准输出和错误输出记录到日志
             logger.info(f"Running test case: {testname}")
             logger.info(result.stdout)
             if result.stderr:
                 logger.error(result.stderr)
         except Exception as e:
-            logger.error(f"An error occurred while running the test case '{testname}': {e}")
+            logger.error(
+                f"An error occurred while running the test case '{testname}': {e}")
 
 
 if __name__ == "__main__":
