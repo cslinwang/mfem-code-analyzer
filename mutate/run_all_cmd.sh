ed1b8bdb8698c3554b574020ee1eee3dcf3e539c
lin wang <wwwglin@163.com>
2023-12-23 14:52:08 +0000

    增加中文脚本

--- mutate/run_all_cmd.sh
+++ mutate/run_all_cmd.sh
@@ -7,6 +7,10 @@ git reset --hard HEAD
 git clean -fd
 git pull
 
+# 如何设置中文
+先更新代码，然后退出screen，然后执行
+cp /root/mfem-code-analyzer/mutate/.screenrc ~/.screenrc
+
 # ocsar10
 # issue3566
 docker run -it -d -p 7001:22 --name myissue3566 cslinwang/mfem:v2.4.1222
